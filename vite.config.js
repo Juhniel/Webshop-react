d68e28d741528c52f5de2c34965f74c9cd6d1278
Juhniel <juhn.sweden@gmail.com>
2023-04-30 01:32:47 +0200

    config for pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const repoBase = process.env.NODE_ENV === 'production' ? '/JAVA22-avjs-react-mp1-Juhn-Kim/' : '/';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: repoBase,
+  build: {
+    outDir: "docs",
+  },
 })
