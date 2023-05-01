c4140c5c44295d13902f44d4681e4aed385449c8
Juhniel <juhn.sweden@gmail.com>
2023-05-02 00:44:26 +0200

    img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
                         "We support fair wages for our employees and local farmers" - Juhn Kim, CEO & Founder of k-pastries
                     </p>
                 </article>
-                <img src="../src/media/img/bakery2.png" alt="cake" className="w-1/2" />
+                <img src="../src/media/img/bakery.png" alt="cake" className="w-1/2" />
             </section>
     )
 }
